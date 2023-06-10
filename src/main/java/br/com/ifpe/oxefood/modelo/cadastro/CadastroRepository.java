4e05c27502a58a2efe868971a1dff589fc242215
Samara-g <119901042+Samara-g@users.noreply.github.com>
2023-06-10 12:56:18 -0300

    deletando

--- src/main/java/br/com/ifpe/oxefood/modelo/cadastro/CadastroRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.ifpe.oxefood.modelo.cadastro;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
-    
-}
