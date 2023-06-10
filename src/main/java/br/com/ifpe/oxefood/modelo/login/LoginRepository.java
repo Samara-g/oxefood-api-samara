7726b983944e1070e1b6697e7a962dd2a1a02bdb
Samara-g <119901042+Samara-g@users.noreply.github.com>
2023-06-10 12:56:38 -0300

    deletando

--- src/main/java/br/com/ifpe/oxefood/modelo/login/LoginRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ifpe.oxefood.modelo.login;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepository extends JpaRepository<Login, Long> {
-
-  
-}
